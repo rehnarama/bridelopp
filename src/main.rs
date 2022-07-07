1e9da81905c8b4d3946caf9830be7e8f8ac63c91
Astrid Rehn <1352555+rehnarama@users.noreply.github.com>
2022-07-07 10:24:24 +0200

    Basic DB support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-#[macro_use] extern crate rocket;
-use rocket_dyn_templates::Template;
-use rocket::serde::{Serialize};
-use rocket::fs::{FileServer};
+#[macro_use]
+extern crate rocket;
 use rocket::form::Form;
+use rocket::fs::FileServer;
+use rocket::http::Status;
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+use rocket_dyn_templates::Template;
+use std::ops::Deref;
+
+use rocket_db_pools::mongodb::{
+    bson::doc,
+    Client,
+};
+use rocket_db_pools::{Connection, Database};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct IndexContext;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Invite {
+    password: String,
+    name: String,
+}
 
 #[derive(Debug, FromForm)]
 struct LoginRequest<'r> {
@@ -18,14 +35,36 @@ fn index() -> Template {
     Template::render("index", IndexContext)
 }
 
+#[derive(Database)]
+#[database("jostrid")]
+pub struct Db(Client);
+
 #[post("/", data = "<request>")]
-fn login<'r>(request: Form<LoginRequest<'r>>) -> &'r str {
-    "blabla"
+async fn login<'r>(
+    request: Form<LoginRequest<'r>>,
+    client: Connection<Db>,
+) -> Result<Json<Invite>, Status> {
+    let invite: Invite = match client
+        .deref()
+        .database("jostrid")
+        .collection::<Invite>("invites")
+        .find_one(doc! { "password": request.password }, None)
+        .await
+    {
+        Ok(invite) => invite.ok_or(Status::NotFound),
+        Err(e) => {
+            println!("Failed to fetch {}", e);
+            Err(Status::InternalServerError)
+        }
+    }?;
+
+    Ok(Json(invite))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(Db::init())
         .mount("/", routes![index, login])
         .mount("/public", FileServer::from("./public"))
         .attach(Template::fairing())
