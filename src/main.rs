c29c7a1767eda17de466c38f20f42283895e55b4
Astrid Rehn <1352555+rehnarama@users.noreply.github.com>
2022-08-21 10:06:13 +0200

    Add basic password/cookie lookup

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,25 @@
 extern crate rocket;
 use rocket::form::Form;
 use rocket::fs::FileServer;
-use rocket::http::Status;
+use rocket::http::{Cookie, CookieJar, Status};
+use rocket::response::Redirect;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
 use rocket_dyn_templates::Template;
 use std::ops::Deref;
 
-use rocket_db_pools::mongodb::{
-    bson::doc,
-    Client,
-};
+use rocket_db_pools::mongodb::{bson::doc, Client};
 use rocket_db_pools::{Connection, Database};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct IndexContext;
+struct LoginContext;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct MainContext {
+    invite: Invite,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -31,8 +35,15 @@ struct LoginRequest<'r> {
 }
 
 #[get("/")]
-fn index() -> Template {
-    Template::render("index", IndexContext)
+async fn index(client: Connection<Db>, cookies: &CookieJar<'_>) -> Result<Template, Status> {
+    match cookies
+        .get("password") {
+            Some(password) => match get_invite(client, password.value().to_string()).await? {
+                Some(invite) => Ok(Template::render("main", MainContext { invite })),
+                None => Err(Status::InternalServerError),
+            },
+            None => Ok(Template::render("login", LoginContext))
+        }
 }
 
 #[derive(Database)]
@@ -43,22 +54,33 @@ pub struct Db(Client);
 async fn login<'r>(
     request: Form<LoginRequest<'r>>,
     client: Connection<Db>,
-) -> Result<Json<Invite>, Status> {
-    let invite: Invite = match client
-        .deref()
-        .database("jostrid")
-        .collection::<Invite>("invites")
-        .find_one(doc! { "password": request.password }, None)
-        .await
-    {
-        Ok(invite) => invite.ok_or(Status::NotFound),
-        Err(e) => {
-            println!("Failed to fetch {}", e);
-            Err(Status::InternalServerError)
+    cookies: &CookieJar<'_>,
+) -> Result<Redirect, Status> {
+    let password = request.password.to_string();
+
+    match get_invite(client, password.clone()).await? {
+        Some(invite) => {
+            cookies.add(Cookie::new("password", password));
+
+            Ok(invite)
+        }
+        None => {
+            println!("Didn't find invite with password {}", password.clone());
+            Err(Status::NotFound)
         }
     }?;
 
-    Ok(Json(invite))
+    Ok(Redirect::to(uri!(index)))
+}
+
+async fn get_invite(client: Connection<Db>, password: String) -> Result<Option<Invite>, Status> {
+    client
+        .deref()
+        .database("jostrid")
+        .collection::<Invite>("invites")
+        .find_one(doc! { "password": password }, None)
+        .await
+        .map_err(|_| Status::InternalServerError)
 }
 
 #[launch]
