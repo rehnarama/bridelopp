899eb841cd3b767305a79d9a5e85e064ee732307
Astrid Rehn <1352555+rehnarama@users.noreply.github.com>
2022-08-21 12:13:52 +0200

    Add basic menu navigation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #[macro_use]
 extern crate rocket;
 use rocket::form::Form;
-use rocket::fs::FileServer;
+use rocket::fs::{FileServer, NamedFile};
 use rocket::http::{Cookie, CookieJar, Status};
 use rocket::response::Redirect;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
 use rocket_dyn_templates::Template;
 use std::ops::Deref;
+use std::path::{Path, PathBuf};
 
 use rocket_db_pools::mongodb::{bson::doc, Client};
 use rocket_db_pools::{Connection, Database};
@@ -20,6 +21,7 @@ struct LoginContext;
 #[serde(crate = "rocket::serde")]
 struct MainContext {
     invite: Invite,
+    route: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -34,16 +36,44 @@ struct LoginRequest<'r> {
     password: &'r str,
 }
 
-#[get("/")]
-async fn index(client: Connection<Db>, cookies: &CookieJar<'_>) -> Result<Template, Status> {
-    match cookies
-        .get("password") {
-            Some(password) => match get_invite(client, password.value().to_string()).await? {
-                Some(invite) => Ok(Template::render("main", MainContext { invite })),
-                None => Err(Status::InternalServerError),
-            },
-            None => Ok(Template::render("login", LoginContext))
-        }
+#[get("/public/<file..>", rank = 1)]
+async fn public(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("public/").join(file)).await.ok()
+}
+
+#[get("/<path..>", rank = 2)]
+async fn index<'r>(
+    path: PathBuf,
+    client: Connection<Db>,
+    cookies: &CookieJar<'_>,
+) -> Result<Template, Status> {
+    let path_str = path.to_str().unwrap_or("main").to_string();
+
+    let resolved = if path_str.is_empty() {
+        "main".to_string()
+    } else if path_str.eq("registration") {
+        path_str
+    } else if path_str.eq("information") {
+        path_str
+    } else if path_str.eq("contact") {
+        path_str
+    } else {
+        return Err(Status::NotFound);
+    };
+
+    match cookies.get("password") {
+        Some(password) => match get_invite(client, password.value().to_string()).await? {
+            Some(invite) => Ok(Template::render(
+                resolved.clone(),
+                MainContext {
+                    invite,
+                    route: resolved.clone(),
+                },
+            )),
+            None => Err(Status::InternalServerError),
+        },
+        None => Ok(Template::render("login", LoginContext)),
+    }
 }
 
 #[derive(Database)]
@@ -70,7 +100,7 @@ async fn login<'r>(
         }
     }?;
 
-    Ok(Redirect::to(uri!(index)))
+    Ok(Redirect::to("/"))
 }
 
 async fn get_invite(client: Connection<Db>, password: String) -> Result<Option<Invite>, Status> {
@@ -87,7 +117,6 @@ async fn get_invite(client: Connection<Db>, password: String) -> Result<Option<I
 fn rocket() -> _ {
     rocket::build()
         .attach(Db::init())
-        .mount("/", routes![index, login])
-        .mount("/public", FileServer::from("./public"))
+        .mount("/", routes![public, index, login])
         .attach(Template::fairing())
 }
